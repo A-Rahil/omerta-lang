3bfe390969482337e7632b3642c8d178e4038b62
Aziz <azizkh797@gmail.com>
2026-04-30 02:31:08 +0400

    Update main.rkt

--- src/main.rkt
+++ src/main.rkt
@@ -22,7 +22,11 @@
   
   (displayln "--- EXECUTION COMPLETE ---"))
 
-#| ;; Test basic variables and math
+;; Test
+
+#|
+
+; Expecting O/P: 11
 (run-omerta "
 the-sitdown {
   cut target = 5 * 2 + 1 !
@@ -31,4 +35,49 @@ the-sitdown {
     holler (target) !
   } !
 }
-") |#
+")
+
+;Expecting O/P: 4100
+(run-omerta "the-sitdown {
+  //between-us Calculating the weekly take
+  cut collections = 5000 !
+  cut expenses = 1200 / 2 + 100 * 3 !
+  cut profit = collections - expenses !
+  
+  holler (\"Weekly Profit:\") !
+  holler (profit) !
+} ")
+
+(run-omerta" the-sitdown {
+  cut sloppy_work = 50 
+  holler (sloppy_work) !
+} ")
+
+; Expecting O/P:
+Wiretap located. Initiating sweep.
+Relocating the crew. Cost:
+15000
+
+(run-omerta" the-sitdown {
+  cut wiretap_found = 1 !
+  
+  say (wiretap_found) {
+    holler (\"Wiretap located. Initiating sweep.\") !
+    cut safe_houses = 3 !
+    
+    //between-us A nested check
+    say (safe_houses) {
+       cut relocation_cost = safe_houses * 5000 !
+       holler (\"Relocating the crew. Cost:\") !
+       holler (relocation_cost) !
+    } !
+  } !
+} ")
+
+; Expecting O/P: Syntax error! Fugazzi token found: HOLLER (value: #f)
+(run-omerta" the-sitdown {
+  cut sloppy_work = 50 
+  holler (sloppy_work) !
+} ")
+
+|#
